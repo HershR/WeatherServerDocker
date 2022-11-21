c91049e5292a299289a58ecee929b416dceeede2
Hersh Rudrawal <45576093+HershR@users.noreply.github.com>
2022-11-20 19:10:53 -0800

    fixed scheduler

--- web/flaskr/tasks.py
+++ web/flaskr/tasks.py
@@ -2,7 +2,7 @@ from .weather import city_ids, update_current_weather, update_forcast_3h
 from .extensions import scheduler
 
 
-#updated the current weather for all cities
+#updated the current weather for all cities every hour
 @scheduler.task('cron', id='update_current_weather_all', minute=0, misfire_grace_time=900)
 def update_current_weather_all():
     print("update current all")
@@ -10,7 +10,8 @@ def update_current_weather_all():
         for id in city_ids():
             update_current_weather(id)
 
-#update the current forecast for all cities
+
+#update the current forecast for all cities once a day
 @scheduler.task('cron', id='update_forecast_all', hour=0, misfire_grace_time=900)
 def update_current_forecast_all():
     print("update forecast all")
--- web/run.py
+++ web/run.py
@@ -3,4 +3,4 @@ from flaskr import create_app
 if __name__ == "__main__":
     app = create_app()
     # alter as seen fit
-    app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=False)
+    app.run(host='0.0.0.0', port=5000, debug=True)
